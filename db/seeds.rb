6fc0ce6a96f8cbd17949289ef3af6decf5328025
ashley <ashley.oketo@student.moringaschool.com>
2022-11-06 22:54:03 +0300

    completed challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,17 +23,7 @@ puts "Creating reviews..."
 # * and a review belongs to an user.                              *
 # *****************************************************************
 # Create Reviews Here
-15.times do 
-    user_ids = User.all.map {|user| user.id}
-    product_ids = Product.all.map {|product| product.id}
 
-    Review.create(
-        user_id: user_ids[rand(0..2)],
-        product_id: product_ids[rand(0..4)],
-        star_rating: rand(1..10),
-        comment: Faker::Lorem.sentence(word_count: rand(1...10))
-    )
-end
 
 
 puts "Seeding done!"
